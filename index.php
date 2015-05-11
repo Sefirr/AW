bf8cde0f9e554b4f36da8bf650bf5a520375444e
Christian <christiangj1992@gmail.com>
2015-05-11 11:37:16 +0200

    Posible estructura con includes

---
+++ index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+
+<html>
+	<head>
+		<link href="style.css" rel="stylesheet" type="text/css" />
+		<meta http-equiv="Content-Type" content="text/html; charset=utf-8"> <!--ESTABLECEMOS LA CODIFICACION A UTF-8-->
+
+		<title>Watch & Comment</title>
+	</head>
+	<body>
+		<div id = "contenedor">
+			<!-- INCLUDE CABECERA -->
+			<?php include'includes/header.php'; ?>
+
+
+			<!-- Menu izq -->
+			<?php include'sidebarIzq.php'; ?>
+			<!-- CONTENIDO -->
+			<div id = "contenido">
+
+			</div>	
+			<!-- INCLUDE FOOTER -->
+			<?php include'includes/footer.php'; ?>
+		</div>		
+	</body>
+</html>
