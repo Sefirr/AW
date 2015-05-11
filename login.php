0b711f0c4871302a287bc142b555a54f80d16584
Christian <christiangj1992@gmail.com>
2015-05-11 12:25:37 +0200

    Ejemplo de vista

---
+++ login.php
@@ -0,0 +1,11 @@
+<?php
+require_once __DIR__.'/includes/config.php';
+require_once __DIR__.'/includes/usuarios.php';
+?>
+<html>
+  <body>
+    <!-- HTML o includes para incluir trozos comunes de HTML -->
+    <?php gestionaFormularioLogin() ?>
+    <!-- Más HTML -->
+ </body>
+</html>
