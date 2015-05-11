ad75ac26f8bf9b849de1907426e7c76f97dfa6e2
Christian <christiangj1992@gmail.com>
2015-05-11 11:19:56 +0200

    subida de principio de includes

---
+++ includes/footer.php
@@ -0,0 +1,12 @@
+<div id="footer">
+	<footer> 
+		<ul>
+			<li> <a href="index.php"> Home</a> </li>
+			<li> <a href="index.php"> Series</a> </li>
+			<li> <a href="index.php"> Películas</a> </li>
+			<li> <a href="index.php"> Videojuegos</a> </li>
+			<li> <a href="#logo"> <img class="flecha" src="img/flecha.png"/></a></li>
+		</ul>
+		<div id="copyright">&copy;Copyright by W&amp;C 2015</div> 
+	</footer>
+</div>
---
+++ includes/header.php
@@ -0,0 +1,16 @@
+<div id="cabecera">
+	<img src= "./img/logo.png" id="logo"/>
+	<div id="LoginAndRegister">
+		<a class="boton"href="login.html">Login</a>
+		<a class="boton" href="register.html">Registro</a>
+	</div>
+	<nav class="menu-cabecera">
+		<ul>
+			<li><a href="index_administrador.html">Home</a></li>
+			<li><a href="peliculas_administrador.html">Películas</li>
+			<li><a href="series_administrador.html">Series</a></li>
+			<li><a href="videojuegos_administrador.html">Videojuegos</a></li>
+			<li><a href="contact_administrador.html">Contacto</a></li>
+		</ul>
+	</nav>
+</div> 
