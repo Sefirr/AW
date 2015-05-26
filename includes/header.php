817d6bce054387070b9dd8098f7cbd2479424c62
Sefir <sefir1223@gmail.com>
2015-05-26 11:56:59 +0200

    Subido header.php

--- includes/header.php
+++ includes/header.php
@@ -1,18 +1,47 @@
+<?php
+	require_once __DIR__.'/config.php';
+	require_once __DIR__.'/procesaUsuario.php';
+?>
+<!DOCTYPE HTML>
+<html>
+<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8"> <!--ESTABLECEMOS LA CODIFICACION A UTF-8-->
+	<meta name="author" content="Watchandcoment Team"><!-- AUTORES DE LA PÁGINA -->
+	<link rel="stylesheet" type="text/css" href="<?php echo RAIZ_APP; ?>css/login.css"><!-- LINK AL ESTILO DE ESTA PAGINA -->
+	<link rel="stylesheet" type="text/css" href="<?php echo RAIZ_APP; ?>css/header.css"><!-- LINK AL ESTILO DE ESTA PAGINA -->
+	<link rel="stylesheet" type="text/css" href="<?php echo RAIZ_APP; ?>css/style.css"><!-- LINK AL ESTILO DE ESTA PAGINA -->
+
+</head>
+<body>
 <div id="cabecera">
-	<img src= "./img/logo.png" id="logo"/>
-	<!--<div id="LoginAndRegister">
-		<a class="boton"href="login.html">Login</a> 
-		<a class="boton" href="register.html">Registro</a>
-	</div>-->
-	
+	<a href ="<?php echo RAIZ_APP; ?>index.php"><img src= "<?php echo RAIZ_APP; ?>/img/logo.png" id="logo"/></a>
+	<div id="LoginAndRegister">
+	<?php  if(isset($_SESSION['usuario'])) {
+	?>
+		<a href="<?php echo RAIZ_APP; ?>includes/logout.php" id="logoutButton" class ="boton">Logout</a>
+	<?php
+		} else {
+	?>
+		<a href="#" id="loginButton" class ="boton">Login</a>
+	<?php } ?>
+		<div id="loginButton">
+		<a class="boton" href="<?php echo RAIZ_APP; ?>includes/register.php">Registro</a></div>
+	</div>
+	<?php 
+		if(!isset($_SESSION['usuario'])) {
+			require_once 'login.php';
+		}			
+	?>
 	<nav class="menu-cabecera">
 		<ul>
-			<li><a href="index_administrador.html">Home</a></li>
-			<li><a href="peliculas_administrador.html">Películas</li>
-			<li><a href="series_administrador.html">Series</a></li>
-			<li><a href="videojuegos_administrador.html">Videojuegos</a></li>
-			<li><a href="contact_administrador.html">Contacto</a></li>
+			<li><a href="<?php echo RAIZ_APP; ?>index.php">Home</a></li>
+			<li><a href="<?php echo RAIZ_APP; ?>includes/peliculas.php">Películas</li>
+			<li><a href="<?php echo RAIZ_APP; ?>includes/series.php">Series</a></li>
+			<li><a href="<?php echo RAIZ_APP; ?>includes/videojuegos.php">Videojuegos</a></li>
+			<li><a href="<?php echo RAIZ_APP; ?>includes/contact.php">Contacto</a></li>
 		</ul>
 	</nav>
-	<?php include'login.php'; ?>
+
 </div> 
+</body>
+</html>
