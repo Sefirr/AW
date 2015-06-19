f5716e3869a7dfa8d8fea18f99c19caafeb9809f
Javier Villarreal Rodríguez <sefir1223@gmail.com>
2015-06-19 17:47:20 +0200

    Corregido error del footer.php

--- includes/footer.php
+++ includes/footer.php
@@ -1,11 +1,12 @@
+<?php require_once __DIR__.'/config.php'; ?>
 <div id="footer">
 	<footer> 
 		<ul>
-			<li> <a href="index.php"> Home</a> </li>
-			<li> <a href="peliculas.php"> Películas</a> </li>
-			<li> <a href="series.php"> Series</a> </li>
-			<li> <a href="videojuegos.php"> Videojuegos</a> </li>
-			<li> <a href="contact.php"> Contacto</a> </li>
+			<li> <a href="<?php echo RAIZ_APP; ?>index.php"> Home</a> </li>
+			<li> <a href="<?php echo RAIZ_APP; ?>peliculas.php"> Películas</a> </li>
+			<li> <a href="<?php echo RAIZ_APP; ?>series.php"> Series</a> </li>
+			<li> <a href="<?php echo RAIZ_APP; ?>videojuegos.php"> Videojuegos</a> </li>
+			<li> <a href="<?php echo RAIZ_APP; ?>contact.php"> Contacto</a> </li>
 			<li id="flecha"> <a href="#logo"> <img class="flecha" src="<?php echo RAIZ_APP; ?>img/flecha.png"/></a></li>	
 			<li> <div id="copyright">&copy;Copyright by W&amp;C 2015</div></li>	
 		</ul>
